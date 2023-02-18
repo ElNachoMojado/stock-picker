fe4124ff59e179631754b3e27659dd1477ab265d
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-18 16:59:12 +0000

    Add stock_picker method that calculates biggest profit only if the day of lowest price is before the one with the highest

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,8 +1,18 @@
 def stock_picker(stock_prices)
+    #Check for the lowest and highest price in the array
     sell = stock_prices.max
     buy = stock_prices.min
-    if stock_prices
+
+    #Places order and sells for the highest profit only if the day of lowest price is before the one with the highest
+    if stock_prices.index(buy) < stock_prices.index(sell)
+        profit = sell - buy
+        puts "For the highest profit of $#{sell} - $#{buy} = $#{profit}, buy stocks at day #{stock_prices.index(buy)} and sell at #{stock_prices.index(sell)}."
+    #else
+
+    end
 end
 
 stock_prices = Array.new(9) {rand(1..40)}
-puts stock_prices
+puts "The prices are #{stock_prices}."
+
+stock_picker(stock_prices)
